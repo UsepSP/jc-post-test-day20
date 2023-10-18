c476b39b7f2ad1d3df72a4a67ddbf322b71f2f9d
UsepSP <usep.sph@gmail.com>
2023-10-18 16:21:39 +0700

    Update Post Test Day ke 20

--- src/main/java/com/juaracoding/Main.java
+++ src/main/java/com/juaracoding/Main.java
@@ -1,9 +1,43 @@
 package com.juaracoding;
 
 
+import com.juaracoding.page.CatatanKeuangan;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Main {
-    public static void main(String[] args) {
+
+        public static void main(String[] args) throws MalformedURLException {
 
 
-    }
+            //DesiredCapabilities
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+            desiredCapabilities.setCapability("deviceName", "SM_M236B");
+            desiredCapabilities.setCapability("udid", "RRCT303G9VB");
+            desiredCapabilities.setCapability("platforName", "android");
+            desiredCapabilities.setCapability("appPackage", "com.chad.financialrecord");
+            desiredCapabilities.setCapability("appActivity", "com.rookie.catatankeuangan.feature.splash.SplashActivity");
+            desiredCapabilities.setCapability("noreset", true);
+
+            //URL
+            URL url = new URL("http://127.0.0.1:4723/wd/hub");
+
+            CatatanKeuangan Catatan = new CatatanKeuangan();
+
+
+            //instance Android driver
+            AndroidDriver driver = new AndroidDriver<>(url, desiredCapabilities);
+
+            //add
+            Catatan.delay(3);
+            Catatan.addIncome("10000","Input Income");
+            System.out.println("Inpus Berhasil");
+
+
+
+
+        }
 }
--- src/main/java/com/juaracoding/page/CatatanKeuangan.java
+++ src/main/java/com/juaracoding/page/CatatanKeuangan.java
@@ -9,16 +9,89 @@ import org.openqa.selenium.support.PageFactory;
 
 public class CatatanKeuangan {
 
+    public static Object addIncome;
     public AndroidDriver<MobileElement> driver;
 
-    public CatatanKeuangan(AndroidDriver<MobileElement>driver){
-        this.driver = driver;
-        PageFactory.initElements(new AppiumFieldDecorator(driver),this);
-    }
+    //Skip PopUp/ Modal
 
-    @AndroidFindBy (id="")
+    @AndroidFindBy (id="com.android.permissioncontroller:id/permission_allow_button")
     private MobileElement akses;
 
+    @AndroidFindBy (id="android:id/button2")
+    private MobileElement popup;
+
+    @AndroidFindBy (xpath="//android.widget.ImageButton[contains(@bounds,\"891\"")
+    private MobileElement btnAdd;
+
+    @AndroidFindBy (id="com.chad.financialrecord:id/btnIncome")
+    private MobileElement btnIncome;
+
+    @AndroidFindBy (id="com.chad.financialrecord:id/etAmount")
+    private MobileElement nominal;
+
+    @AndroidFindBy (id="com.chad.financialrecord:id/etAmount")
+    private MobileElement note;
+
+    @AndroidFindBy (id="com.chad.financialrecord:id/btSave")
+    private MobileElement btnSave;
+
+    @AndroidFindBy (id="com.chad.financialrecord:id/btnExpense")
+    private MobileElement btnCost;
+
+    @AndroidFindBy(id = "//android.widget.TextView[contains(@bounds,\"922\")]")
+    private MobileElement resultIncome;
+
+    @AndroidFindBy(id = "//android.widget.TextView[contains(@bounds,\"922\")]")
+    private MobileElement resultCost;
+
+
+
+
+
+
+
+    //Method
+
+    public void addIncome(String nominal, String note){
+        delay(7);
+        akses.click();
+        delay(2);
+        popup.click();
+        btnAdd.click();
+        delay(2);
+        btnIncome.click();
+        this.nominal.sendKeys(nominal);
+        this.note.sendKeys(note);
+        btnSave.click();
+        delay(2);
+    }
+
+
+    public void addCost(String nominal, String note){
+        btnCost.click();
+        btnAdd.click();
+        btnIncome.click();
+        this.nominal.sendKeys(nominal);
+        this.note.sendKeys(note);
+        btnSave.click();
+    }
+
+    public String getTxtResultIncome() {
+        return resultIncome.getText();
+    }
+
+    public String getTxtResultCost() {
+        return resultCost.getText();
+    }
+
+
+    public void delay (long detik) {
+        try {
+            Thread.sleep(detik * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
 
